6a363d0c8a4b49da04ef045ee7437c73ceda6dde
YoungHwa Shin <sinyh104@e-mail.hs.kr>
2016-09-27 20:59:42 +0900

    #1 Make Draw Shape

---
+++ app/src/main/java/kr/hs/emirim/sinyh104/touchgrapic/MainActivity.java
@@ -0,0 +1,29 @@
+package kr.hs.emirim.sinyh104.touchgrapic;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new DrawShape(getApplicationContext()));
+    }
+    class DrawShape extends View {
+        DrawShape(Context context){
+            super(context);
+        }
+        protected void onDraw(Canvas canvas){
+            super.onDraw(canvas);
+            float cx=getWidth()/2.0f;
+            float cy=getHeight()/2.0f;
+            Paint paint=new Paint();
+            canvas.drawCircle(cx,cy,50,paint);
+        }
+    }
+}

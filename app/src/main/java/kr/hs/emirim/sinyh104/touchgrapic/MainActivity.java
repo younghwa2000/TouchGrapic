775b7b88e5f563be51cf4f9ae99ea089dfc9223b
YoungHwa Shin <sinyh104@e-mail.hs.kr>
2016-09-29 19:21:45 +0900

    #3 Edit MainActivity and Layout

--- app/src/main/java/kr/hs/emirim/sinyh104/touchgrapic/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sinyh104/touchgrapic/MainActivity.java
@@ -13,7 +13,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new DrawShape(getApplicationContext()));
+        setContentView(R.layout.activity_main);
+        //
+    }
+
+    public void drawShape(View v){
+
     }
     class DrawShape extends View {
         DrawShape(Context context){
